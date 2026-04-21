37b3d482bb94bf7548149f3d026439cc88a647fe
Darius Mercadier <dmercadier@google.com>
2026-04-21 08:56:29 +0200

    [v8] Stop fuzzing typer & typed optimizations
    Bug: 435589244
    Change-Id: I1a8d2dd8ec1014a6bcf6c34d8c2c17409257d948
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/9223176
    Auto-Submit: Darius Mercadier <dmercadier@google.com>
    Reviewed-by: Matthias Liedtke <mliedtke@google.com>
    Commit-Queue: Matthias Liedtke <mliedtke@google.com>

--- Sources/Fuzzilli/Profiles/V8CommonProfile.swift
+++ Sources/Fuzzilli/Profiles/V8CommonProfile.swift
@@ -1157,10 +1157,6 @@ public func v8ProcessArgs(randomize: Bool, forSandbox: Bool) -> [String] {
         }
     }
 
-    if probability(0.1) {
-        args.append("--turboshaft-typed-optimizations")
-    }
-
     if probability(0.7) {
         args.append("--turbolev")
         if probability(0.82) {
@@ -1246,9 +1242,6 @@ public func v8ProcessArgs(randomize: Bool, forSandbox: Bool) -> [String] {
         if (!args.contains("--no-maglev") || args.contains("--turbolev")) && probability(0.1) {
             // TODO(tacet): update the Turbolev conditions to !args.contains("--no-turbolev") after Turbolev trial
             args.append("--maglev-assert-types")
-        }
-        if probability(0.1) {
-            args.append("--turboshaft-assert-types")
         }
         if probability(0.2) {
             args.append("--turboshaft-verify-load-elimination")
