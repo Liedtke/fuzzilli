97a19a7906d3aba8742e508f018d3523a3822455
Leon Bettscheider <bettscheider@google.com>
2026-04-28 08:30:22 +0000

    Fix ExplorationMutator crash on switch labels
    ExplorationMutator may explore inner outputs, which adds instrumentation
    code immediately. However, such arbitrary JS code is not allowed in the
    outer switch context, before a `case` was seen.
    
    In general, exploring labels probably does not make sense, so this CL
    avoids all labels from being explored.
    
    Bug: 504947197
    Change-Id: I9ed4b30e12fb277cc4136ec9d4edfa1c0e2166e9
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/9241640
    Reviewed-by: Matthias Liedtke <mliedtke@google.com>
    Commit-Queue: Leon Bettscheider <bettscheider@google.com>

--- Sources/Fuzzilli/Mutators/ExplorationMutator.swift
+++ Sources/Fuzzilli/Mutators/ExplorationMutator.swift
@@ -71,7 +71,11 @@ public class ExplorationMutator: RuntimeAssistedMutator {
             guard !(instr.op is WasmOperation || instr.op is WasmTypeOperation) else { continue }
 
             for v in instr.allOutputs {
-                if b.type(of: v) == .jsAnything || b.type(of: v) == .unknownObject {
+                let type = b.type(of: v)
+                if !type.Is(.jsAnything) {
+                    // Skip .jsBlockLabel, .jsLoopLabel, ...
+                    continue
+                } else if type == .jsAnything || type == .unknownObject {
                     untypedVariables.append(v)
                 } else {
                     typedVariables.append(v)
